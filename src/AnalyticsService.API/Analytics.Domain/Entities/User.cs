e75bb9309c61e732287cceb2baffcf6938b40d30
DejviFiala <david.fiala.44@gmail.com>
2025-04-08 11:37:50 +0200

    Created User entity

---
+++ src/AnalyticsService.API/Analytics.Domain/Entities/User.cs
@@ -0,0 +1,17 @@
+﻿using Analytics.Domain.Enums;
+
+namespace Analytics.Domain.Entities
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public Guid RoleId { get; set; }
+        public string State { get; set; } // e.g. "active", "inactive", "banned"
+        public Sex Sex { get; set; }
+        public int Weight { get; set; } // in grams
+        public int Height { get; set; } // in mm
+        public DateTime BirthDate { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime LastUpdated { get; set; }
+    }
+}
